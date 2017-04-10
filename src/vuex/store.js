a099f3f8fb88ea65f7d64f1d73bb2f48aa2645ce
ben.luo <516801125@qq.com>
2017-04-10 16:54:30 +0800

    vuex demo

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -5,6 +5,7 @@ import { ADD_ONE, SUBTRACT_ONE } from './mutation-types';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    strict: process.env.NODE_ENV !== 'production',
     state: {
         count: 0,
     },
